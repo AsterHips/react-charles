a0419e73a597d7f822ab0f339f651080246be786
Charles Burnatowski <charles.burna@hotmail.fr>
2023-08-23 01:00:17 +0200

    Carroussel fonctionnel

--- src/components/Carroussel.jsx
+++ src/components/Carroussel.jsx
@@ -1,46 +1,64 @@
-import Logement_img from '../assets/sources_images/Logement.png';
-import arrow_left from '../assets/sources_images/arrow_left.png';
-import arrow_right from '../assets/sources_images/arrow_right.png';
-import { useEffect, useState } from 'react';
+import arrow_right from "../assets/sources_images/arrow_right.png";
+import arrow_left from "../assets/sources_images/arrow_left.png";
+import { useEffect, useState } from "react";
+import React from 'react';
+
+const arrowRightElement = document.getElementsByClassName(arrow_right);
+const arrowLeftElement = document.getElementsByClassName(arrow_left);
+
 
 function Carroussel({ appart }) {
-  const [index, setIndex] = useState(0);
+    const [index, setIndex] = useState(0);
+    const galleryLength = appart.pictures.length;
+          
+      function NavigateLeft(){
+        console.log(index)
+        console.log({index})
+        if (index <=0) {
+          setIndex(galleryLength-1);
+        } else {
+          setIndex(index - 1);
+        }
+      }
+      
+      console.log(index)
+      console.log({index})
+      
+      function NavigateRight(){
+        console.log(index)
+        console.log({index})
+        if (index >=galleryLength-1) {
+          setIndex(0);
+        } else {
+          setIndex(index + 1);
+        }
+      }
 
-  return (
-    <div id="gallery-logement">
-      <img src={arrow_left} alt="fleche gauche" className="arrow arrow_left" />
-      <img
-        src={appart.pictures[index]}
-        alt="gallerie photos"
-        className="gallery-logement-current"
-      />
-      <img
-        src={arrow_right}
-        alt="fleche droite"
-        className="arrow arrow_right"
-      />
-      <span className="pages">{/* {index}/{Number(pictures.length-1)} */}</span>
-    </div>
-  );
+
+    return (
+        <div id="gallery-logement">
+            <img
+                src={arrow_left}
+                alt="fleche gauche"
+                className="arrow arrow_left"
+                onClick={NavigateLeft}
+            />
+            <img
+                src={appart.pictures[index]}
+                alt="gallerie photos"
+                className="gallery-logement-current"
+            />
+            <img
+                src={arrow_right}
+                alt="fleche droite"
+                className="arrow arrow_right"
+                onClick={NavigateRight}
+            />
+            <span className="pages">
+                {index + 1}/{appart.pictures.length}
+            </span>
+        </div>
+    );
 }
-
-// arrowRightElement.addEventListener('click', () => {
-
-//   if (index >= logements.length-1) {
-//     index = 0
-//   } else {
-//     index++
-//   }
-//   bannerImageElement.src = `./assets/images/logementshow/${logements[index].image}`;
-// });
-
-// arrowLeftElement.addEventListener('click', () => {
-//   if (index <= 0) {
-//     index = logements.length-1
-//   } else {
-//     index--
-//   }
-//   bannerImageElement.src = `./assets/images/logementshow/${logements[index].image}`;
-// });
 
 export default Carroussel;
