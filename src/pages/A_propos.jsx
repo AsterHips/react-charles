a023b71ad43caba7985da05fbae2406adacae318
Charles Burnatowski <charles.burna@hotmail.fr>
2023-12-06 05:56:42 +0100

    textes mis à jour

--- src/pages/A_propos.jsx
+++ src/pages/A_propos.jsx
@@ -3,17 +3,23 @@ import Dropdown from "../components/Dropdown";
 
 function A_propos() {
     document.title = "À propos";
-    const contenuTemp =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel consequat tortor. In hac habitasse platea dictumst. Vestibulum vitae mauris eget sem sollicitudin rhoncus non at odio.";
+    const Fiabilité =
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.";
+    const Respect =
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.";
+    const Service =
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.";
+    const Sécurité =
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.";
 
     return (
         <>
             <Banner />
             <div className="a_propos">
-                <Dropdown dropdownTitle="Fiabilité" content={contenuTemp} />
-                <Dropdown dropdownTitle="Respect" content={contenuTemp} />
-                <Dropdown dropdownTitle="Service" content={contenuTemp} />
-                <Dropdown dropdownTitle="Sécurité" content={contenuTemp} />
+                <Dropdown dropdownTitle="Fiabilité" content={Fiabilité} />
+                <Dropdown dropdownTitle="Respect" content={Respect} />
+                <Dropdown dropdownTitle="Service" content={Service} />
+                <Dropdown dropdownTitle="Sécurité" content={Sécurité} />
             </div>
         </>
     );
